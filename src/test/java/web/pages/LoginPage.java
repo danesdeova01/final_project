a005e94f4eea6cbd34394966f77755b5d85b77bb
danesdeova01 <151486791+danesdeova01@users.noreply.github.com>
2025-08-03 09:50:17 +0700

    Update LoginPage.java

--- src/test/java/web/pages/LoginPage.java
+++ src/test/java/web/pages/LoginPage.java
@@ -20,6 +20,12 @@ public class LoginPage {
     @FindBy(css = "h3[data-test='error']")
     private WebElement errorMessage;
 
+    @FindBy(id = "react-burger-menu-btn")
+    private WebElement menuButton;
+
+    @FindBy(id = "logout_sidebar_link")
+    private WebElement logoutLink;
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
@@ -41,5 +47,20 @@ public class LoginPage {
 
     public String getErrorMessage() {
         return errorMessage.getText();
+    }
+
+    public void clickMenuButton() {
+        menuButton.click();
+    }
+
+    public void clickLogout() {
+
+        // Tunggu beberapa saat jika perlu agar menu muncul
+        try {
+            Thread.sleep(500); // opsional, bisa diganti dengan WebDriverWait
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        logoutLink.click();
     }
 }
